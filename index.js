14ff8148ed24523cdbcf5a63e137230a89905009
CoderJ <jixin1006@gmail.com>
2018-02-05 13:54:42 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const Koa = require('koa');
+const app = new Koa();
+const config = require(`${__dirname}/ecosystem.json`);
+app.use(async ctx => {
+    let event = ctx.headers['X-GitHub-Event']
+    let delivery = ctx.headers['X-GitHub-Delivery']
+    let signature = ctx.headers['X-Hub-Signature']
+    ctx.body = {
+        event,
+        delivery,
+        signature,
+        config,
+        path: ctx.path,
+        data : ctx.request.body
+    };
+});
+
+app.listen(8000);
